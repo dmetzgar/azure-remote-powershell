aada0b4cc84f5c8b5c7343e3e3005a6552758097
dmetzgar <dmetzgar@microsoft.com>
2014-09-30 23:29:47 -0700

    Fixed nuget package references.

---
+++ AzurePerfTools.PowerShellWindowsService/InstallRemotePS.bat
@@ -0,0 +1,3 @@
+%windir%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe /u AzurePerfTools.PowerShellWindowsService.exe
+%windir%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe AzurePerfTools.PowerShellWindowsService.exe
+net start AzureRemotePowerShell
